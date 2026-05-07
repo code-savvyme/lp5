dd7662f8cb7a66910710e9ca672dd96caa0ecf8f
code-savvyme <snvisa347@gmail.com>
2026-05-07 18:44:32 +0530

    Update and rename 2_parallel_merge.cpp to hpc2bubblemerge.cpp

--- hpc2bubblemerge.cpp
+++ hpc2bubblemerge.cpp
@@ -4,35 +4,32 @@
 using namespace std;
 
 void merge(int arr[], int low, int mid, int high) {
-    // Create arrays of left and right partititons
     int n1 = mid - low + 1;
     int n2 = high - mid;
 
     int left[n1];
     int right[n2];
-    
-    // Copy all left elements
-    for (int i = 0; i < n1; i++) left[i] = arr[low + i];
-    
-    // Copy all right elements
-    for (int j = 0; j < n2; j++) right[j] = arr[mid + 1 + j];
-    
-    // Compare and place elements
+
+    for (int i = 0; i < n1; i++)
+        left[i] = arr[low + i];
+
+    for (int j = 0; j < n2; j++)
+        right[j] = arr[mid + 1 + j];
+
     int i = 0, j = 0, k = low;
 
     while (i < n1 && j < n2) {
-        if (left[i] <= right[j]){
+        if (left[i] <= right[j]) {
             arr[k] = left[i];
             i++;
-        } 
-        else{
+        }
+        else {
             arr[k] = right[j];
             j++;
         }
         k++;
     }
 
-    // If any elements are left out
     while (i < n1) {
         arr[k] = left[i];
         i++;
@@ -62,6 +59,7 @@ void parallelMergeSort(int arr[], int low, int high) {
                 parallelMergeSort(arr, mid + 1, high);
             }
         }
+
         merge(arr, low, mid, high);
     }
 }
@@ -69,8 +67,10 @@ void parallelMergeSort(int arr[], int low, int high) {
 void mergeSort(int arr[], int low, int high) {
     if (low < high) {
         int mid = (low + high) / 2;
+
         mergeSort(arr, low, mid);
         mergeSort(arr, mid + 1, high);
+
         merge(arr, low, mid, high);
     }
 }
@@ -78,25 +78,60 @@ void mergeSort(int arr[], int low, int high) {
 int main() {
     int n = 10;
     int arr[n];
+
     double start_time, end_time;
 
-    // Create an array with numbers starting from n to 1.
-    for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
-    
-    // Measure Sequential Time
-    start_time = omp_get_wtime(); 
-    mergeSort(arr, 0, n - 1);
-    end_time = omp_get_wtime(); 
-    cout << "Time taken by sequential algorithm: " << end_time - start_time << " seconds\n";
+    for (int i = 0, j = n; i < n; i++, j--)
+        arr[i] = j;
 
-    // Reset the array
-    for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
-    
-    //Measure Parallel time
-    start_time = omp_get_wtime(); 
+    start_time = omp_get_wtime();
+    mergeSort(arr, 0, n - 1);
+    end_time = omp_get_wtime();
+
+    cout << "Time taken by sequential algorithm: "
+         << end_time - start_time << " seconds\n";
+
+    for (int i = 0, j = n; i < n; i++, j--)
+        arr[i] = j;
+
+    start_time = omp_get_wtime();
     parallelMergeSort(arr, 0, n - 1);
-    end_time = omp_get_wtime(); 
-    cout << "Time taken by parallel algorithm: " << end_time - start_time << " seconds";
-    
+    end_time = omp_get_wtime();
+
+    cout << "Time taken by parallel algorithm: "
+         << end_time - start_time << " seconds";
+
+    return 0;
+}
+
+
+
+
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+
+        #pragma omp parallel for
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 4, 3, 2, 1};
+    int n = 5;
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
